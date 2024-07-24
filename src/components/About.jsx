d84a7a23b9fbaf63708b98ae76821e0949e00603
CHILUKA SUNNY <82013394+sunny-star@users.noreply.github.com>
2024-07-25 01:43:16 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div name="About" className='w-full bg-gradient-to-b from-gray-800 to-black md:px-20 text-white '>
 
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full '>
-        <div className='pb-5 py-3'>
+        <div className='pb-3 py-1'>
           <p className='text-3xl  text-yellow-300  font-bold inline border-b-4 border-gray-500'>
             About
           </p>
