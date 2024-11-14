50a88ac040d7536d72d314883bed871867897153
CHILUKA SUNNY <82013394+sunny-star@users.noreply.github.com>
2024-11-14 12:19:11 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
           </button>
         </>
       ),
-      href: 'https://drive.google.com/file/d/1MPSMMUXRFxO7gqVITWVeAXISLnS--eJh/view?usp=drivesdk',
+      href: 'https://drive.google.com/file/d/12czQrYUszLFVvUU-vBywO3sv3LNC8ufU/view?usp=drivesdk',
       style: 'rounded-tr-md',
       download: false, // No download attribute needed
     },
